3a4e77c907ac996fed67f9f6622aa224885bb0ae
Edward Chan <chan@sikho.ca>
2009-10-06 23:05:03 +0900

    initial loading of world compete

--- world/world.clj
+++ world/world.clj
@@ -9,23 +9,26 @@
 	{ :classname "org.sqlite.JDBC"
 	  :subprotocol "sqlite"
 	  :subname "world.db" })
-	
+
+(def *user-possessions-atom* (atom {}))
 
 (defn init-items []
 	(with-connection *db* (with-query-results results ["select name from items"]
 		(reduce (fn [items row] (assoc items (keyword (:name row)) 0)) {} results))))
 
 (defn load-users []
-	(let [items-ref (ref (init-items))]
+	(let [items (init-items)]
 		(with-connection *db* (with-query-results results ["select distinct owner from possessions"]
-			(reduce (fn [users row] (assoc users (keyword (:owner row)) items-ref)) {} results)))))	
-
-(def *user-possessions-atom* (atom (load-users)))
+			(reduce (fn [users row] (assoc users (keyword (:owner row)) (ref items))) {} results)))))	
 
 (defn load-possessions []
+	(reset! *user-possessions-atom* (load-users))
 	(with-connection *db* (with-query-results results ["select owner, item, sum(qty) as qty, max(timestamp) as timestamp from possessions group by owner, item"]
 		(doseq [row results]
 			(let [user (keyword (:owner row))
 			      item (keyword (:item row))
 			      qty (:qty row)
-			      m (user @*user-possessions-atom*)])))))
+			      user-possession-ref (user @*user-possessions-atom*)]
+				(dosync (commute user-possession-ref
+						 (fn [m item qty] (assoc m item qty))
+						 item qty)))))))
