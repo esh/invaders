39cf7e2c993de2951d1f45c3055a6585f62026e2
Edward Chan <chan@sikho.ca>
2009-10-06 22:34:00 +0900

    nice refactoring

--- world/world.clj
+++ world/world.clj
@@ -10,18 +10,22 @@
 	  :subprotocol "sqlite"
 	  :subname "world.db" })
 	
-(def *user-possessions-map* (atom {}))
+
+(defn init-items []
+	(with-connection *db* (with-query-results results ["select name from items"]
+		(reduce (fn [items row] (assoc items (keyword (:name row)) 0)) {} results))))
+
+(defn load-users []
+	(let [items-ref (ref (init-items))]
+		(with-connection *db* (with-query-results results ["select distinct owner from possessions"]
+			(reduce (fn [users row] (assoc users (keyword (:owner row)) items-ref)) {} results)))))	
+
+(def *user-possessions-atom* (atom (load-users)))
 
 (defn load-possessions []
-	(let [results (with-connection *db* (with-query-results results ["select name from items"]))
-	      items (reduce (fn [items row] (assoc items (keyword (:name row)) 0)) {} results)]
-		(with-connection *db* (with-query-results results ["select owner, item, sum(qty) as qty, max(timestamp) as timestamp from possessions group by owner, item"]
-			(doseq [row results]
-				(let [user (keyword (:owner row))
-			      	      item (keyword (:item row))
-				      qty (:qty row)]
-					(if (not (contains? @*user-possessions-map* user))
-              					(let [m (assoc @*user-possessions-map* user (ref {}))]  
-							(reset! *user-possessions-map* m)))
-						(let [m (user @*user-possessions-map*)]
-							(println row))))))))
+	(with-connection *db* (with-query-results results ["select owner, item, sum(qty) as qty, max(timestamp) as timestamp from possessions group by owner, item"]
+		(doseq [row results]
+			(let [user (keyword (:owner row))
+			      item (keyword (:item row))
+			      qty (:qty row)
+			      m (user @*user-possessions-atom*)])))))
