3535db5c99dfa5c92177d2bd4f0a31e0e79b98f2
Edward Chan <esh@mini-esh.(none)>
2009-10-25 12:55:44 +0900

    commiting schema

---
+++ dispatcher/clients.sql
@@ -0,0 +1,5 @@
+BEGIN TRANSACTION;
+CREATE TABLE clients (user, uid, last, status);
+INSERT INTO "clients" VALUES('esh','esh',1256439935.12445,'online');
+INSERT INTO "clients" VALUES('yuki','yuki',1256049131.76034,'online');
+COMMIT;
