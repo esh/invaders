7b002b66e9c0aa32ea1ecd968f009b9367d1edab
Edward Chan <esh@mini-esh.(none)>
2009-10-27 23:35:33 +0900

    populated in memory grid with contents of universe

--- dispatcher/www/controller/universe.js
+++ dispatcher/www/controller/universe.js
@@ -1,15 +1,42 @@
 dispatcher.register("universe", function() {
 	$("#main").load("/view/universe.html", null, function(res, status, req) {
+		var universe = new Array()
+
 		$("#market").click(function() {
+			mq.unsubscribe("universe")
 			dispatcher.run("market")
 		})
 		$("#ark_ship").click(function() {
+			mq.unsubscribe("universe")
 			dispatcher.run("ark_ship")
 		})
 
 		mq.subscribe("universe", function(data) {
+			function extract_key(key) {
+				key = key.substring(1, key.length - 1)
+				key = key.split(" ")
+				return { x: parseInt(key[0]), y: parseInt(key[1]) }
+			}
+
+			delete data["type"]
+			// find highest row/col
+			var x_max = 0
+			var y_max = 0
 			for(var key in data) {
-				if(key != "type") $("#pane").append(key + ": " + data[key] + "&nbsp;")
+				key = extract_key(key)
+				if(key.x > x_max) x_max = key.x
+				if(key.y > y_max) y_max = key.y
+			}
+		
+			// create grid
+			for(var y = 0 ; y <= y_max ; y++) {
+				universe[y] = new Array(x_max)
+			}
+
+			// populate the grid
+			for(var key in data) {
+				var t = extract_key(key)
+				universe[t.y][t.x] = data[key]
 			}
 		})
 
