f0f12f075c6c74f36469fc7bf0b1ea3f0e958bdd
Edward Chan <esh@mini-esh.(none)>
2009-11-03 21:11:17 +0900

    refactored dispatcher out

--- universe/universe.clj
+++ universe/universe.clj
@@ -1,8 +1,5 @@
 (ns universe
 	(:use [clojure.contrib.sql :only (with-connection with-query-results)])
-	(:use [clojure.contrib.json.read])
-	(:use [clojure.contrib.json.write])
-	(:use [clojure.walk])
 	(:require [amqp]))
 
 (. Class (forName "org.sqlite.JDBC"))
@@ -65,21 +62,3 @@
 				(keys possessions)
 				(map deref (vals possessions)))
 			:type "possessions"))))
-
-
-(defmulti dispatch #(keyword (:action %)))
-(defmethod dispatch :possessions [msg] (get-possessions (:user msg)))
-(defmethod dispatch :universe [msg] (get-universe))
-
-;listen to universe
-(let [conn (amqp/connect "localhost" 5672 "guest" "guest" "/")
-      chan (amqp/create-channel conn "ex" "topic")]
-	(amqp/declare-queue chan "universe")
-	(amqp/bind-queue chan "universe" "ex" "universe")
-	(amqp/subscribe chan "universe"
-		(fn [msg]
-			(let [msg (keywordize-keys (read-json-string msg))
-			      user (:user msg)
-			      res (dispatch msg)]
-				(println res)
-				(amqp/publish chan "ex" (str "client." user) (json-str res)))))) 
