566f0646e597b7d2225daa96c74f4f345496a4a4
Edward Chan <esh@mini-esh.(none)>
2009-11-07 18:41:08 +0900

    game loop runs every minute

--- universe/game.clj
+++ universe/game.clj
@@ -2,14 +2,16 @@
 	(:require [amqp])
 	(:use [clojure.contrib.json.write]))
 
-(defn gameloop []
-	(let [conn (amqp/connect "localhost" 5672 "guest" "guest" "/")
-      	      chan (amqp/create-channel conn "ex" "topic")]
-		(do
+(let [conn (amqp/connect "localhost" 5672 "guest" "guest" "/")
+      chan (amqp/create-channel conn "ex" "topic")]
+	(.start (new Thread
+		#((loop []
 			(universe/mine-resources)
 			(doseq [user (universe/get-online-users)] 
 				(amqp/publish
 					chan
 					"ex"
 					(str "client." user) 
-					(json-str (universe/get-possessions user)))))))
+					(json-str (universe/get-possessions user))))
+			(. Thread sleep 60000)	
+			(recur))))))
