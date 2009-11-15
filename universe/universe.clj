8f71f9294f104f1dab4309e851d465880b087acb
Edward Chan <esh@mini-esh.(none)>
2009-11-16 01:06:29 +0900

    refactoring

--- universe/universe.clj
+++ universe/universe.clj
@@ -11,13 +11,6 @@
 	:subprotocol "sqlite"
 	:subname "../db/clients.db"})
 
-(defn table-to-map [db table-name]
-	(with-connection db (with-query-results results [(str "select * from " table-name)] 
-		(let [v (map (fn [r] (assoc r :type table-name)) results)
-		      k (map (fn [r] [(:x r) (:y r)]) v)
-		      v (map (fn [r] (zipmap (rest (rest (keys r))) (rest (rest (vals r))))) v)]
-			(zipmap k v)))))
-
 (def *users* (with-connection *user-db* (with-query-results results ["select user from clients"]
 	(reduce (fn [items row] (conj items (keyword (:user row)))) [] results)))) 
 
@@ -36,21 +29,24 @@
 					*items*
 					(map ref (replicate (count *items*) 0))))))))
 
-(def *ships-ref* (ref (table-to-map *universe-db* "ships")))
+(def *ships-atom*
+	(atom (with-connection *universe-db* (with-query-results results ["select * from ships"]
+		(doall (map #(ref (assoc % :type "ships")) results))))))
 
-(def *resources-ref* (ref (table-to-map *universe-db* "resources")))
-
+(def *resources-atom*
+	(with-connection *universe-db* (with-query-results results ["select * from resources"] 
+		(atom (reduce (partial merge-with into) (doall (map (fn [r] {[(:x r) (:y r)] [r]}) results)))))))
+ 
 (defn get-online-users []
 	(with-connection *user-db* (with-query-results results ["select user from clients where status='online'"]
 		(reduce (fn [items row] (conj items (:user row))) [] results)))) 
  
 (defn get-universe []
-	(dosync (merge-with
-			(fn [a b]
-				(let [a (if (list? a) a [a])
-				      b (if (list? b) b [b])]
-					(into a b))) 
-					@*resources-ref* @*ships-ref*)))
+	(dosync 
+		(let [ships (map deref @*ships-atom*)
+		      k (map (fn [r] [(:x r) (:y r)]) ships)
+		      ships (zipmap k (map (fn [r] [r]) ships))]
+			(merge-with into @*resources-atom* ships))))
 
 (defn get-possessions [user]
 	(dosync (let [possessions ((keyword user) @*possessions-atom*)]
@@ -64,23 +60,17 @@
 		      val (type (user @*possessions-atom*))]
 			(commute val + n))))
 
-(defn step-ship [pos] 
-	(dosync (let [ship (get @*ships-ref* pos)
-		      res (get @*resources-ref* pos)]
-			(if (not (nil? res))
-				(let [owner (:owner ship)
-				      item (:item res)
-		      		      yield (:yield res)
-				      timestamp (. System currentTimeMillis)]
-					(update-possessions owner item yield)		
-					(with-connection *universe-db*
-						(insert-rows "possessions" [owner item yield timestamp])))))))
-
-(defn move-ship [source dest]
-	(dosync (if (contains? @*ships-ref* source)
-			(let [ship (get @*ships-ref* source)]
-				(ref-set *ships-ref*
-					(assoc (dissoc @*ships-ref* source) dest ship))))))
+;(defn step-ship [pos] 
+;       (dosync (let [ship (get @*ships-ref* pos)
+;       	      res (get @*resources-ref* pos)]
+;       		(if (not (nil? res))
+;       			(let [owner (:owner ship)
+;       			      item (:item res)
+;       	      		      yield (:yield res)
+;       			      timestamp (. System currentTimeMillis)]
+;       				(update-possessions owner item yield)		
+;       				(with-connection *universe-db*
+;       					(insert-rows "possessions" [owner item yield timestamp])))))))
 
 (with-connection *universe-db* 
 	(with-query-results results ["select owner, item, sum(qty) as qty, max(timestamp) as timestamp from possessions group by owner, item"]
